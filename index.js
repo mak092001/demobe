5c533093ad4d2b04c6c144d5b082249e4e43b6be
mak092001 <private.mak09@gmail.com>
2021-11-30 16:46:12 +0530

    op

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ let express = require("express");
 let app = express();
 require("dotenv").config();
 let mango = require("mongoose");
-let bp = require("body-parser");
 let cors = require("cors");
+let bp = require("body-parser");
 let getschema = require("./schema.js");
 app.use(express.static("public"));
 app.use(bp.urlencoded({ extended: true }));
 app.use(bp.json());
-app.use(cors());
+app.use(cors);
+const allowedorigin = ["http://localhost:3000"];
+const options = (cors.CorsOptions = {
+  origin: allowedorigin,
+});
+app.use(cors(options));
 mango
   .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
