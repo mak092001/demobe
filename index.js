753f53dfeb860e7296628b22886de7859ef203b5
mak092001 <private.mak09@gmail.com>
2021-11-30 21:36:59 +0530

    last

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.use(express.static("public"));
 app.use(bp.urlencoded({ extended: true }));
 app.use(bp.json());
 app.use(cors());
-// const allowedorigin = ["http://localhost:3000"];
-// const options = (cors.CorsOptions = {
-//   origin: allowedorigin,
-// });
-// app.use(cors(options));
+const allowedorigin = ["http://localhost:3000"];
+const options = (cors.CorsOptions = {
+  origin: allowedorigin,
+});
+app.use(cors(options));
 mango
   .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
